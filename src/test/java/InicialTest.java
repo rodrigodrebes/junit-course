922f8708c708008b004dfeb3d1d39d109236c840
Rodrigo <rodrigodrebes97@gmail.com>
2023-08-10 13:41:08 -0300

    JUnit initial configuration

--- src/test/java/InicialTest.java
+++ src/test/java/InicialTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InicialTest {
+    @Test
+    public void verificarJUnit(){
+        Assertions.assertTrue(true);
+    }
 }
